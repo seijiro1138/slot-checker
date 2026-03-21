d024fad51f807dd326783d0189bddb4320a6b8ee
seijiro1138 <kode.takayo.seiya.121@gmail.com>
2026-03-21 12:11:31 +0900

    Fix package name in AccessLogRepository

--- src/main/java/com/example/slot_checker/AccessLogRepository.java
+++ src/main/java/com/example/slot_checker/AccessLogRepository.java
@@ -1,6 +1,8 @@
-package com.example.slotchecker;
+package com.example.slot_checker;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccessLogRepository extends JpaRepository<AccessLog, Long> {
 }
