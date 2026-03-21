8c23d2cbd8d11137fc5f807c6a0725b43289a514
seijiro1138 <kode.takayo.seiya.121@gmail.com>
2026-03-21 14:44:47 +0900

    Update AccessLogRepository.java

--- src/main/java/com/example/slot_checker/AccessLogRepository.java
+++ src/main/java/com/example/slot_checker/AccessLogRepository.java
@@ -1,8 +1,19 @@
 package com.example.slot_checker;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface AccessLogRepository extends JpaRepository<AccessLog, Long> {
+
+    // 最新20件「以外」を削除する特注SQL
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM access_log WHERE id NOT IN (" +
+                   "SELECT id FROM access_log ORDER BY access_time DESC LIMIT 20)", 
+           nativeQuery = true)
+    void deleteOldLogs();
 }
